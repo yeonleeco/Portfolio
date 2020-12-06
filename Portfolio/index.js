9e9894f109bf4eb8ec3307152cdef167ac112599
yeonleeco <45839144+yeonleeco@users.noreply.github.com>
2020-12-06 14:42:22 -0700

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,11 @@
+$('.hover-border').mouseover(function() {
+    $('.bolder1').css('font-weight','bolder');
+    $('.bolder2').css('font-weight','normal');});
+
+
+    $('.hover-border').mouseout(function() {
+        $('.bolder1').css('font-weight','normal');
+         $('.bolder2').css('font-weight','bolder')
+        });
+
+
